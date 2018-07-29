b10efa2854a6b3e184e4dcc4eb420d0b2dd67b44
romebake <romebake@gmail.com>
2018-07-29 18:43:46 +0800

    feat: update README.md and license

--- Imooc_Auto_Next_Media.user.js
+++ Imooc_Auto_Next_Media.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         ImoocAutoNextMedia - 慕课网自动播放下一节视频
 // @namespace    https://greasyfork.org/
-// @version      0.1.0
+// @version      0.1.1
 // @date         2018-07-29
 // @author       romebake
 // @blog         https://www.romebake.com
 // @code         https://github.com/romebake/ImoocAutoNextMedia
 // @description  慕课网自动播放下一节视频
+// @license      MIT; https://opensource.org/licenses/MIT
 // @match        *://*.imooc.com/*
 // @icon         https://www.imooc.com/favicon.ico
 // @grant        none
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-###慕课网自动播放下一节视频
+# 慕课网自动播放下一节视频
+[安装脚本]
+
+本脚本参考并使用了"[慕课网自动播放下一节视频脚本]"的部分代码
+
+  [慕课网自动播放下一节视频脚本]: https://greasyfork.org/zh-CN/scripts/39217-慕课网自动播放下一节视频脚本
+  [安装脚本]: https://greasyfork.org/zh-CN/scripts/370710-imoocautonextmedia-慕课网自动播放下一节视频
